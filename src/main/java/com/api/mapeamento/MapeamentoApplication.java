4e90d6698f1b753d66c267df03f346938eb26463
viniciuscfn <c.viniciusmagalhaes@gmail.com>
2022-11-04 09:18:19 -0300

    adicionando

---
+++ src/main/java/com/api/mapeamento/MapeamentoApplication.java
@@ -0,0 +1,13 @@
+package com.api.mapeamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapeamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapeamentoApplication.class, args);
+	}
+
+}
